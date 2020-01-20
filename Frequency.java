475513cb9b35ec40b0e4f869072f77eb24bcd5c4
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-01-20 05:45:12 +0530

    initialise the array

--- Frequency.java
+++ Frequency.java
@@ -1,5 +1,9 @@
 public class Frequency {
     public static void main(String[] args) {
+        //Initialise Array
+        int[] arr = new int[]{1, 2, 8, 3, 2, 2, 2, 5, 1};
+        //Array fr will store frequencies of element
+        int[] fr = new int[arr.length];
 
     }
 }
