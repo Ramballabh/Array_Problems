7ea21d5ac75d0cf7630506b0fe7e9d771fed515d
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-01-20 06:07:23 +0530

    take input n from the user using scanner class

--- RotateLeft.java
+++ RotateLeft.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class RotateLeft {
     public static void main(String[] args) {
         //initialise array
         int[] arr = new int[]{1, 2, 3, 4, 5};
+        //n determine the number of times an array should be rotates
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
 
     }
