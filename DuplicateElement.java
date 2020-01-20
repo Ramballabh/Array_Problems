dde245bd9b2034d9687eef8aa640821759f26bb4
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-01-20 06:31:12 +0530

    print the duplicate element

--- DuplicateElement.java
+++ DuplicateElement.java
@@ -2,7 +2,13 @@ public class DuplicateElement {
     public static void main(String[] args) {
         //Initialise Array
         int[] arr = new int[]{1, 3, 4, 4, 5, 6, 7, 8, 8, 9};
-
-
+        System.out.println("Duplicate elements in given array : ");
+        //Search for duplicate element
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j])
+                    System.out.println(arr[j]);
+            }
+        }
     }
 }
