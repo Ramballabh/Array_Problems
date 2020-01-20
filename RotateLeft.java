d936fc62b97c991562660866d184327094ff7004
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-01-20 06:21:41 +0530

    Rotate the given array by n times toward left

--- RotateLeft.java
+++ RotateLeft.java
@@ -12,7 +12,17 @@ public class RotateLeft {
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i] + " ");
         }
-
-
+        //Rotate the given array by n times toward left
+        for (int i = 0; i < n; i++) {
+            int j, first;
+            //Store the first element of the array
+            first = arr[0];
+            for (j = 0; j < arr.length - 1; j++) {
+                //Shift the element of array by one
+                arr[j] = arr[j + 1];
+            }
+            //add first element at the end of array
+            arr[j] = first;
+        }
     }
 }
