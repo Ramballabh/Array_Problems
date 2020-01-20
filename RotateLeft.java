9c8082fda634f5e5a9dc4947860fe6b4d30883c3
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-01-20 06:11:14 +0530

    Display original array

--- RotateLeft.java
+++ RotateLeft.java
@@ -7,6 +7,11 @@ public class RotateLeft {
         //n determine the number of times an array should be rotates
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        //Display original array
+        System.out.println("Original array");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
 
 
     }
