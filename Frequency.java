73ac5e90e2511b56e55a3ed8605725fbb5de5fa8
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-01-20 05:53:33 +0530

    Count the frequency of each element in an array using loop

--- Frequency.java
+++ Frequency.java
@@ -4,6 +4,18 @@ public class Frequency {
         int[] arr = new int[]{1, 2, 8, 3, 2, 2, 2, 5, 1};
         //Array fr will store frequencies of element
         int[] fr = new int[arr.length];
-
+        int visited = -1;
+        for (int i = 0; i < arr.length; i++) {
+            int count = 1;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                    //To avoid counting same element again
+                    fr[j] = visited;
+                }
+            }
+            if (fr[i] != visited)
+                fr[i] = count;
+        }
     }
 }
